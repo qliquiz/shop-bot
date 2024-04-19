babd6f65e089945060b5f991bdbccd9984285340
qliquiz <artgor005@gmail.com>
2024-04-20 00:40:10 +0800

    f5

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from '../Button/Button';
 import './Header.css';
-import { useTelegram } from '../../hooks/useTelegram';
+import { useTelegram } from '../hooks/useTelegram';
 
 const Header = () => {
     const {user, onClose} = useTelegram();
