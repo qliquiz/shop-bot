19921c76486c42ddbf2c289af02168eed5f77fb4
qliquiz <artgor005@gmail.com>
2024-04-19 19:09:49 +0800

    2

--- src/components/hooks/useTelegram.js
+++ src/components/hooks/useTelegram.js
@@ -1,10 +1,10 @@
 const tg = window.Telegram.WebApp;
 
 export function useTelegram() {
-    function onClose() {tg.close()}
+    const onClose = () => {tg.close()}
 
-    function onToggleButton() {
-        if (tg.MainButton.isVisiable) tg.MainButton.hide();
+    const onToggleButton = () => {
+        if (tg.MainButton.isVisible) tg.MainButton.hide();
         else tg.MainButton.show();
     }
 
