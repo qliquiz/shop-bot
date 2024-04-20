28f4244b05c951dce250c77173acf18da19e3565
qliquiz <artgor005@gmail.com>
2024-04-20 13:11:48 +0800

    hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { useEffect } from 'react';
-import { useTelegram } from './components/hooks/useTelegram';
+import { useTelegram } from './hooks/useTelegram';
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import ProductList from './components/ProductList/ProductList';
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import './Form.css';
-import { useTelegram } from "../hooks/useTelegram";
+import { useTelegram } from "../../hooks/useTelegram";
 
 
 const Form = () => {
