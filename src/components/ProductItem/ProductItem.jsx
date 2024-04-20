d347acb60e79fb6037b5b88fa0344044007415c0
qliquiz <artgor005@gmail.com>
2024-04-20 14:50:03 +0800

    font3

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -14,7 +14,7 @@ const ProductItem = ({product, className, onAdd}) => {
             <div className={'title'}>{product.title}</div>
             <div className={'description'}>{product.description}</div>
             <div className={'price'}>
-                <span>Стоимость: {product.price}₽</span>
+                Стоимость: <span className='price-number'>{product.price}</span>₽
             </div>
             <Button className={'add-btn'} onClick={onAddHandler}>
                 Добавить в корзину
