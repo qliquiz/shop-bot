6247412ba1c70a8b211d6f37833b38f91d58ad90
qliquiz <artgor005@gmail.com>
2024-04-20 14:04:00 +0800

    font1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import Button from "../Button/Button";
+// import Button from "../Button/Button";
 import {useTelegram} from "../../hooks/useTelegram";
 import './Header.css';
 
 const Header = () => {
-    const {user, onClose} = useTelegram();
+    const {user} = useTelegram();
 
     return (
         <div className={'header'}>
-            <h1>Colonochka Corporation</h1>
+            <h3>Colonochka Corporation</h3>
             <span className={'username'}>
                 {user?.username}
             </span>
