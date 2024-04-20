8c278cc4873e47d5fe1178446af9e21d4b7121e1
qliquiz <artgor005@gmail.com>
2024-04-20 14:44:01 +0800

    font-size and shadows

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -14,7 +14,7 @@ const ProductItem = ({product, className, onAdd}) => {
             <div className={'title'}>{product.title}</div>
             <div className={'description'}>{product.description}</div>
             <div className={'price'}>
-                <span>Стоимость: <b>{product.price}</b></span>
+                <span>Стоимость: {product.price}₽</span>
             </div>
             <Button className={'add-btn'} onClick={onAddHandler}>
                 Добавить в корзину
